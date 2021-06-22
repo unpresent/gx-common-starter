e0170db5d26bdc131bca6dcdc49bcb6d9db8d490
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-06-22 14:15:21 +0300

    v1.0.3. Доработки AbstractSettingsController

--- src/main/java/ru/gagarkin/gxfin/common/settings/AbstractSettingsController.java
+++ src/main/java/ru/gagarkin/gxfin/common/settings/AbstractSettingsController.java
@@ -9,15 +9,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-public class AbstractSettingsController implements SettingsController {
+public abstract class AbstractSettingsController implements SettingsController {
+    /**
+     * Передается в поле settingName объекта-события об изменении настройки, если поменялось слишком много настроек.
+     */
     public static final String ALL = "*";
 
     @Getter(AccessLevel.PROTECTED)
     private final ApplicationContext context;
 
+    /**
+     * Список насроек со значениями
+     */
     @Getter(AccessLevel.PROTECTED)
     private final Map<String, Object> settings;
 
+    /**
+     * Объект-событие об изменении настройки/настроек
+     */
     @Getter(AccessLevel.PROTECTED)
     private final SettingsChangedEvent settingsChangedEvent;
 
@@ -25,14 +34,33 @@ public class AbstractSettingsController implements SettingsController {
         super();
         this.context = context;
         this.settings = new HashMap<>();
-        this.settingsChangedEvent = new SettingsChangedEvent(this, ALL);
+        this.settingsChangedEvent = createSettingsChangedEvent();
     }
 
+    /**
+     * @return Создается объект-событие, которое будет использоваться для вызова события об изменении настройки/настроек.
+     * Можно переопределять в наследниках, чтобы объект-событие был наследником от SettingsChangedEvent
+     * @see SettingsChangedEvent
+     */
+    protected SettingsChangedEvent createSettingsChangedEvent() {
+        return new SettingsChangedEvent(this, ALL);
+    }
+
+    /**
+     * Получение значения настройки по ее имени.
+     * @param settingName имя настройки.
+     * @return значение настройки.
+     */
     @Override
     public Object getSetting(String settingName) {
         return this.settings.get(settingName);
     }
 
+    /**
+     * Изменение значения настройки.
+     * @param settingName имя настройки.
+     * @param value новое значение настройки.
+     */
     @Override
     public void setSetting(String settingName, Object value) {
         var oldValue = this.settings.get(settingName);
