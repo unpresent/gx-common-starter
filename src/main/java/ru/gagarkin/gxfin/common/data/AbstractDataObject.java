7f55ea5959f5f7a4d32ee63c5970ef02012d1e53
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-07-02 16:31:35 +0300

    v1.0.9. Добавил implements Serializable в AbstractDataObject.

--- src/main/java/ru/gagarkin/gxfin/common/data/AbstractDataObject.java
+++ src/main/java/ru/gagarkin/gxfin/common/data/AbstractDataObject.java
@@ -1,7 +1,9 @@
 package ru.gagarkin.gxfin.common.data;
 
+import java.io.Serializable;
+
 /**
  * Базовый тип для DTO (объектов передачи данных).
  */
-public abstract class AbstractDataObject implements DataObject {
+public abstract class AbstractDataObject implements DataObject, Serializable {
 }
