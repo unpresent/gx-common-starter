a6f37e3a5e954327a90528068ed0d2e1de03e3b3
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-10-20 18:17:12 +0300

    v1.1.11. Исправил константы SimpleSettingsController.

--- src/main/java/ru/gx/settings/SimpleSettingsController.java
+++ src/main/java/ru/gx/settings/SimpleSettingsController.java
@@ -3,16 +3,12 @@ package ru.gx.settings;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
+import ru.gx.worker.AbstractWorker;
 
 import javax.annotation.PostConstruct;
 
 @SuppressWarnings("unused")
 public class SimpleSettingsController extends AbstractSettingsController {
-    String WAIT_ON_STOP_MS = "wait_on_stop_ms";
-    String WAIT_ON_RESTART_MS = "wait_on_restarts_ms";
-    String MIN_TIME_PER_ITERATION_MS = "min_time_per_iteration_ms";
-    String TIMEOUT_RUNNER_LIFE_MS = "timeout_runner_life_ms";
-
     @Getter
     @NotNull
     private final String serviceName;
@@ -34,9 +30,9 @@ public class SimpleSettingsController extends AbstractSettingsController {
 
     @PostConstruct
     public void init() throws UnknownApplicationSettingException {
-        this.loadIntegerSetting(this.serviceName + "." + WAIT_ON_STOP_MS);
-        this.loadIntegerSetting(this.serviceName + "." + WAIT_ON_RESTART_MS);
-        this.loadIntegerSetting(this.serviceName + "." + MIN_TIME_PER_ITERATION_MS);
-        this.loadIntegerSetting(this.serviceName + "." + TIMEOUT_RUNNER_LIFE_MS);
+        this.loadIntegerSetting(this.serviceName + "." + AbstractWorker.settingSuffixWaitOnStopMs);
+        this.loadIntegerSetting(this.serviceName + "." + AbstractWorker.settingSuffixWaitOnRestartMs);
+        this.loadIntegerSetting(this.serviceName + "." + AbstractWorker.settingSuffixMinTimePerIterationMs);
+        this.loadIntegerSetting(this.serviceName + "." + AbstractWorker.settingSuffixTimoutRunnerLifeMs);
     }
 }
