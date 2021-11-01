1f1d935fd80b78ce32aebd25b887d6d05e47da7a
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-11-01 19:21:21 +0300

    v1.1.12++. Переработаны события и их использование.

--- src/main/java/ru/gx/utils/BytesUtils.java
+++ src/main/java/ru/gx/utils/BytesUtils.java
@@ -4,33 +4,23 @@ import java.nio.ByteBuffer;
 
 @SuppressWarnings("unused")
 public class BytesUtils {
-    private static ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+    private static ByteBuffer bufferLong = ByteBuffer.allocate(Long.BYTES);
 
     public static byte[] longToBytes(long x) {
-        buffer.putLong(0, x);
-        return buffer.array();
+        bufferLong.putLong(0, x);
+        return bufferLong.array();
     }
 
     public static long bytesToLong(byte[] bytes) {
         return ByteBuffer.wrap(bytes).getLong();
-        /*
-        buffer.put(bytes, 0, bytes.length);
-        buffer.flip();//need flip
-        return buffer.getLong();
-        */
     }
 
     public static byte[] intToBytes(int x) {
-        buffer.putInt(0, x);
-        return buffer.array();
+        bufferLong.putInt(0, x);
+        return bufferLong.array();
     }
 
     public static long bytesToInt(byte[] bytes) {
         return ByteBuffer.wrap(bytes).getInt();
-        /*
-        buffer.put(bytes, 0, bytes.length);
-        buffer.flip();//need flip
-        return buffer.getInt();
-        //*/
     }
 }
